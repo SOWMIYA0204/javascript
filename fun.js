ca91f15961bc086b1cab830dc523c27f948119f7
SOWMIYA0204 <135124221+SOWMIYA0204@users.noreply.github.com>
2023-11-11 04:26:55 +0530

    Javascript concepts

---
+++ fun.js
@@ -0,0 +1,62 @@
+console.log("HII SOWMI");
+var name="Sowmi";
+var name="Sindhu";
+let name1="Inba";
+name1="Midhun";
+let good;
+console.log(name)
+console.log(name1)
+console.log(good)
+ 
+
+
+function drivingtest(age){
+    if(age>18){
+        var msg="eligible";
+    }else{
+        var msg="not eligible";
+    }
+    console.log("you are" + msg);
+}
+drivingtest(14);
+
+
+var a1=4;
+var a2="4";
+console.log(a1==a2);
+console.log(a1===a2);
+  
+
+var a=10;
+var b=20;
+console.log(a+b)
+
+//ex1.js
+var price=93;
+var product="apple";
+var tax=10;
+console.log(product)
+var total=price+tax;
+console.log(total)
+
+//ex2.js
+var FruitName="apple";
+var count=10;
+var price=25;
+var total=count*price;
+console.log(FruitName)
+console.log(total)
+ 
+//increment,decrement
+var a=10;
+var b=++a;
+console.log(b)
+console.log(a)
+
+//function call
+function ps(){
+    console.log("sowmi")
+}ps()
+
+
+
