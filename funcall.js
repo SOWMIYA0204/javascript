8929c04c5967062622c16622e4ec185236399468
SOWMIYA0204 <135124221+SOWMIYA0204@users.noreply.github.com>
2023-11-11 19:19:51 +0530

    function call

---
+++ funcall.js
@@ -0,0 +1,77 @@
+function redmi(){
+    console.log("this is redmi")
+}
+function samsung(){
+    console.log("this is samsung")
+}
+function vivo()
+{
+    console.log("this is vivo")
+}
+redmi()
+vivo()
+//called function only displayed
+
+var a=10;
+var b=20;
+function add(){
+    console.log(add=a+b)
+}add()
+
+//create a variable called favorite actress,favourite player and favorite movie
+//create a function called favorite()
+var factress="Priyanka Chopra";
+var fplayer="Cristiano Ronaldo";
+var fmovie="Sita Ramam";
+ 
+function favourite(){
+    console.log("factress:"+"Priyankachpra")
+    console.log("fplayer:"+"Cristiano Ronaldo")
+    console.log("fmovie:"+"Sita Ramam")
+}favourite()
+
+//parameters
+function add(a,b){
+    console.log(a+b)
+}add()
+
+//area
+function area(l,b)
+{
+    var a=l*b;
+    console.log("the area is:"+a)
+}area(10,300)
+
+//return type
+function myname(){
+    return"Sowmi"
+}
+var a=myname()
+console.log(a)
+
+
+function add(a,b){
+    return(a+b)
+}
+ var a=add(10,20)
+ console.log(a)
+
+ //if else
+ var rain=false
+ if(rain)
+ {
+    console.log("Take an umbrella")
+ }
+ else
+ {
+    console.log("Enjoy the sunshine")
+ }
+
+ //if ex2
+ var homework=done
+ if(homework){
+    console.log("Great Job")
+ }
+ else{
+    console.log("finish your before you are playing")
+ }
