4235da681796d3a1119582f04cdfa02af615996b
SOWMIYA0204 <135124221+SOWMIYA0204@users.noreply.github.com>
2023-11-14 16:34:31 +0530

    Add files via upload

---
+++ ifelse.js
@@ -0,0 +1,53 @@
+ //if else
+ //ex1
+ var rain=false
+ if(rain)
+ {
+    console.log("Take an umbrella")
+ }
+ else
+ {
+    console.log("Enjoy the sunshine")
+ }
+
+
+
+ //if ex2
+ var homework=true
+ if(homework){
+    console.log("Great Job")
+ }
+ else{
+    console.log("finish your before you are playing")
+ }
+
+ //if ex3
+ var cookie=true
+ if(cookie)
+ {
+    console.log("would you like a cookie")
+ }
+ else{
+    console.log("time to bake more cookies")
+ }
+
+ //if ex4
+ var teeth=false
+ if(teeth)
+ {
+    console.log("your teeth are clean")
+ }
+ else{
+    console.log("go brush your teeth now")
+ }
+
+ //if ex5
+ var birthday=false
+ if(birthday)
+ {
+    console.log("Happy birthday,enjoy your special day")
+ }
+ else
+ {
+    console.log("Have a great day")
+ }
